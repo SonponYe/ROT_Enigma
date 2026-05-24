ee13e8244f2f3802cc93e128c134ea2f596973c7
SonponYe-shuaChief <sonponyeshua@gmail.com>
2026-05-24 20:10:57 +0000

    additions

--- rot_enigma/app/components/footer.jsx
+++ rot_enigma/app/components/footer.jsx
@@ -3,6 +3,7 @@ function footer ()  {
         <footer className="footer">
             <p>Copyright &copy; 2024 ROT_Enigma. All rights reserved.</p>
             <p>Sonpon Ye-shua Chief</p>
+            
         </footer>
     )
 }
